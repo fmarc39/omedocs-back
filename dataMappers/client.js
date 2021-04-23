291578351a73578ebfe57e290544112e308431bb
Zachary Bournand <zacharybournand@gmail.com>
2021-04-23 15:01:15 +0200

    PG_URL à DATABASE_URL

--- dataMappers/client.js
+++ dataMappers/client.js
@@ -3,9 +3,9 @@
 const { Pool } = require('pg');
 
 // Créer une nouvelle connexion à la base de données en créant un nouvel objet Pool avec l'URL de notre base de données 
-// (stocké dans la variable PG_URL)
+// (stocké dans la variable DATABASE_URL)
 const client = new Pool({
-    connectionString: process.env.PG_URL,
+    connectionString: process.env.DATABASE_URL,
     // SSL (Secure Sockets Layer) crypte les communications client/serveur pour améliorer la sécurité
     ssl: {
         // On ne vérifie pas l'identité du serveur 

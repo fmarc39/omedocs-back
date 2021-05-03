cbd879a91c1acc28bd01bf474c7c3aca7c8f8835
Zachary Bournand <zacharybournand@gmail.com>
2021-05-03 12:00:41 +0200

    Ajout orders et resetPassword

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -1,68 +1,3 @@
-// On importe les fonctions du fichier editInventoryDataMapper
-const { selectOrderNumbers, insertOrder, insertOrderProductRelation, selectOrders } = require('../dataMappers/orderDataMapper');
-// On récupère la librairie 'randomstring' qui nous crée une chaîne de caractères aléatoire
-const randomstring = require('randomstring');
-
-// On export nos fonctions
-module.exports = {
-    // 
-    async createOrder (request, response, next) {
-        let generateNumber = randomstring.generate({ length: 17 });
-
-        const { price, pharmacyname, hospitalid, productid, quantityToBuy } = request.body;
-
-        try  {
-            const orderNumbers = await selectOrderNumbers();
-
-            if (orderNumbers) {
-                for (let number of orderNumbers) {
-                    console.log('number :', number);
-
-                    const obj = Object.values(number);
-                    const value = obj[0];
-
-                    if (value === generateNumber) {
-                        generateNumber = randomstring.generate({ length: 17 });
-                    };
-                };  
-            }
-                
-            const newOrder = await insertOrder(generateNumber, price, pharmacyname, hospitalid);
-            const newLink = await insertOrderProductRelation(productid, newOrder.id, quantityToBuy);
-
-            response.status(200).json({ newOrder, newLink });
-                
-            /* if (!orderNumbers) {
-                const newOrder = await insertOrder(generateNumber, price, pharmacyname, hospitalid);
-                const newLink = await insertOrderProductRelation(productid, newOrder.id, quantityToBuy);
-
-                response.status(200).json({ newOrder, newLink });
-            } else {
-                for (let number of orderNumbers) {
-                    console.log('number :', number);
-
-                    const obj = Object.values(number);
-                    const value = obj[0];
-
-                    if (value === generateNumber) {
-                        generateNumber = randomstring.generate({ length: 17 });
-                    };
-                };  
-            
-                const newOrder = await insertOrder(generateNumber, price, pharmacyname, hospitalid);
-                const newOrderProductRelation = await insertOrderProductRelation(productid, newOrder.id, quantityToBuy);
-
-                response.status(200).json({ newOrder, newOrderProductRelation });
-            } */
-        // S'il y a une erreur au niveau du serveur, on renvoit le statut d'erreur 500
-        } catch(error) {
-            next(error);
-        }
-    }, 
-
-    async getOrders (request, response, next) {
-        const userId = request.params.id;
-=======
 // On importe les fonctions du fichier orderDataMapper
 const { selectOrderNumbers, insertOrder, insert_order_product_relation, selectOrders } = require('../dataMappers/orderDataMapper');
 // On récupère la librarie 'randomstring' qui crée une chaîne de caractères aléatoire
@@ -100,21 +35,18 @@ module.exports =  {
                 newOrder, 
                 order_product_relation
             }); 
-
         } catch(error) {
             next(error);
         }
     },
  
     async getOrders (request, response, next) {
-        const { userId } = request.params;
->>>>>>> order
+        const userId = parseInt(request.params.userId, 10);
 
         try {
             const orders = await selectOrders(userId);
 
             if (!orders) {
-<<<<<<< HEAD
                 next()
             } else {
                 response.status(200).json({ 
@@ -122,22 +54,8 @@ module.exports =  {
                     orders 
                 }); 
             }
-=======
-                next();
-            } else {
-                response.status(200).json({ 
-                    status: "success",
-                    orders
-                });
-            }
-
->>>>>>> order
         } catch(error) {
             next(error);
         }
     }
-<<<<<<< HEAD
 }
-=======
-}
->>>>>>> order
--- dataMappers/orderDataMapper.js
+++ dataMappers/orderDataMapper.js
@@ -1,22 +1,15 @@
-<<<<<<< HEAD
 // Récupère le pool de clients PostgreSQL
 const client = require('./client');
 
 // On export les fonctions
 module.exports = {
     // Sélectionne depuis la bdd les numéros de commande
-=======
-const client = require('./client');
-
-module.exports = {
->>>>>>> order
     async selectOrderNumbers() {
         const result = await client.query(`
             SELECT order_number
                 FROM "order"`
         );
 
-<<<<<<< HEAD
         // S'il n'y en a pas, on revoit "undefined"
         if (result.rowCount === 0 ) {
             return undefined;
@@ -40,7 +33,7 @@ module.exports = {
     },
 
     // Insère et sélectionne (avec "RETURNING") les données reliant une commande à un produit et sa quantité achetée
-    async insertOrderProductRelation(product_id, order_id, quantityToBuy) {
+    async insert_order_product_relation(product_id, order_id, quantityToBuy) {
         const result = await client.query(`
             INSERT INTO product_has_order (product_id, order_id, quantity_bought)
                 VALUES ($1, $2, $3)
@@ -54,48 +47,14 @@ module.exports = {
 
     // Sélectionne depuis la bdd les données des commandes de l'utilisateur
     async selectOrders(buyer_id) {
-=======
-        return result.rows;
-    },
-
-    async insertOrder(orderNumber, totalCost, pharmacyName, buyerId) {
-        const result = await client.query(`
-            INSERT INTO "order" (order_number, total_cost, pharmacy_name, buyer_id)
-                VALUES ($1, $2, $3, $4)
-            RETURNING *`,
-            [orderNumber, totalCost, pharmacyName, buyerId]
-        );
-
-        return result.rows[0];
-    },
-
-    async insert_order_product_relation(productId, orderId, quantityToBuy) {
-        const result = await client.query(`
-            INSERT INTO product_has_order
-                VALUES ($1, $2, $3)
-            RETURNING *`,
-            [productId, orderId, quantityToBuy]
-        );
-
-        return result.rows[0];
-    },
-
-    async selectOrders(buyerId) {
->>>>>>> order
         const result = await client.query(`
             SELECT * 
                 FROM "order"
                     WHERE buyer_id=$1`,
-<<<<<<< HEAD
                     [buyer_id]
         );
 
         // Renvoit ces données
-=======
-            [buyerId]
-        );
-
->>>>>>> order
         return result.rows;
     }
 }
